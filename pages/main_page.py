9f1e5063cb4ffd6fccccdb544d41201ea1b9ed54
Dragonaer <loseva_3@mail.ru>
2026-02-12 19:09:31 +0300

    making changes to the steps allure in pages/main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,18 +5,18 @@ from curl import url_dzen
 
 
 class MainPage(BasePage):
-    @allure.title("Кликнуть на логотип Яндекса")
+    @allure.step("Кликнуть на логотип Яндекса")
     def click_on_logo_yandex(self):
         self.click_on_element(MainPageLocators.YA_LOGO_BUTTON)
         self.driver.switch_to.window(self.driver.window_handles[-1])
         self.wait_for_page(url_dzen)
 
-    @allure.title("Кликнуть на логотип Самокат")
+    @allure.step("Кликнуть на логотип Самокат")
     def click_on_logo_scooter(self):
         self.click_on_element(MainPageLocators.ORDER_BUTTON_HEADER_MAINPAGE)
         self.click_on_element(MainPageLocators.SCOOTER_LOGO_BUTTON) 
 
-    @allure.title("Кликнуть на вопрос из раздела Вопросы о важном")
+    @allure.step("Кликнуть на вопрос из раздела Вопросы о важном")
     def click_on_question(self, heading):
         self.click_on_element(heading)
 
