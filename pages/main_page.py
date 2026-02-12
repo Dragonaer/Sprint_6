51be1d7b0e1bed15a6bc343205f37860b592dad8
Dragonaer <loseva_3@mail.ru>
2026-02-12 19:06:14 +0300

    changing the url in the pages/main_page.py folder

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 import allure
 from pages.base_page import BasePage
 from locators.main_page_locators import MainPageLocators
-
+from curl import url_dzen
 
 
 class MainPage(BasePage):
@@ -9,7 +9,7 @@ class MainPage(BasePage):
     def click_on_logo_yandex(self):
         self.click_on_element(MainPageLocators.YA_LOGO_BUTTON)
         self.driver.switch_to.window(self.driver.window_handles[-1])
-        self.wait_for_page('https://dzen.ru/?yredirect=true')
+        self.wait_for_page(url_dzen)
 
     @allure.title("Кликнуть на логотип Самокат")
     def click_on_logo_scooter(self):
